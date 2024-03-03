cc92278020f7da5f5eedfcb9cd4e26dd168e6ee2
Jon Way <jway9422@gmail.com>
2024-03-03 08:59:52 -0500

    Auth fix

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http,Headers } from '@angular/http';
 
 import { Trip } from '../models/trip';
 import { User } from '../models/user';
@@ -19,8 +19,13 @@ export class TripDataService {
 
   public addTrip(formData: Trip): Promise<Trip> {
     console.log('Inside TripDataService#addTrip');
+    const httpOptions = {
+      headers: new Headers({
+        'Authorization': `Bearer ${this.storage.getItem('travlr-token')}`
+      })
+    };
     return this.http
-      .post(this.tripUrl, formData) // pass form data in request body
+      .post(this.tripUrl, formData, httpOptions) // pass form data in request body
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
@@ -47,8 +52,13 @@ export class TripDataService {
   public updateTrip(formData: Trip): Promise<Trip> {
     console.log('Inside TripDataService#updateTrip');
     console.log(formData);
+    const httpOptions = {
+      headers: new Headers({
+        'Authorization': `Bearer ${this.storage.getItem('travlr-token')}`
+      })
+    };
     return this.http
-      .put(this.tripUrl + formData.code, formData)
+      .put(this.tripUrl + formData.code, formData, httpOptions)
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
